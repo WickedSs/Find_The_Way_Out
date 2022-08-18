057c837971466b3538a4d22dfea6f1ff4fd79da5
WickedSs <slimane.ricardo@gmail.com>
2022-08-18 01:22:32 +0100

    a better idea to reduce harcoded sprites

--- Level.py
+++ Level.py
@@ -17,13 +17,31 @@ VISITED = []
 DIM = 15
 
 
-class Sprite(pygame.sprite.Sprite):
-    def __init__(self, group, x, y, image):
-        super().__init__(group)
-        self.x, self.y = x, y
+class Tile:
+    def __init__(self, x, y, image, edges):
         self.image = image
+        self.x, self.y = x, y
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = self.x, self.y
+        self.rect.x, self.rect.y = self.x * TILE_SIZE, self.y * TILE_SIZE
+        self.edges = edges
+        
+    def rotate(self, angle):
+        new_image = pygame.transform.rotate(self.image, 90 * angle)
+        new_edges = []
+        length = self.edges.length;
+        for i in range(length):
+            new_edges[i] = self.edges[(i - angle + length) % length];
+        return Tile(self.x, self.y, new_image, new_edges)
+    
+    def flip(self, x_axis, y_axis):
+        new_image = pygame.transform.flip(self.image, x_axis, y_axis)
+        new_edges = self.edges
+        if x_axis:
+            new_edges[3], new_edges[1] = new_edges[1], new_edges[3]
+        if y_axis:
+            new_edges[0], new_edges[2] = new_edges[2], new_edges[0]
+            
+        return Tile(self.x, self.y, new_image, new_edges)
 
 
 class Cell:
@@ -32,6 +50,9 @@ class Cell:
         self.collapsed = False
         self.options = [sprite for sprite in SPRITES]
     
+    def rotate(self):
+        return
+    
     def set_options(self, options):
         self.options = options
     
@@ -39,7 +60,7 @@ class Cell:
 
 class Level:
     spritesheet_filename = os.path.join(ROOT, "Assets/Spritesheet.png")
-    example_file_01 = os.path.join(ROOT, "Assets\Example-01.png")
+    example_file_01 = os.path.join(ROOT, "Assets/Example-01.png")
     def __init__(self):
         global SPRITES
         self.display_surface = pygame.display.get_surface()
@@ -47,62 +68,20 @@ class Level:
         self.sprite_sheet_rect = self.sprite_sheet.get_rect()
         self.level_sprites = pygame.sprite.Group()
         self.layout_in_use = "First_Layout"
-        SPRITES = [                                                                         #TOP     RIGHT       DOWN      LEFT 
-            { "Index" : 0, "Image" : self.get_sprite((0 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [1, 2], 2 : [11], 3 : [] } },
-            { "Index" : 1, "Image" : self.get_sprite((1 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [1, 2, 6], 2 : [0, 1, 6], 3 : [12] } },
-            { "Index" : 2, "Image" : self.get_sprite((2 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [13], 3 : [0, 1] } },
-            { "Index" : 3, "Image" : self.get_sprite((4 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [14], 3 : [] } },
-            { "Index" : 4, "Image" : self.get_sprite((6 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [12], 1 : [24], 2 : [13, 46], 3 : [12] } },
-            { "Index" : 5, "Image" : self.get_sprite((7 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [12], 1 : [12], 2 : [11, 45], 3 : [24] } },
-            { "Index" : 6, "Image" : self.get_sprite((9 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [3], 1 : [1], 2 : [12], 3 : [1] } },
-            { "Index" : 7, "Image" : self.get_sprite((10 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [13], 1 : [36, 32], 2 : [13], 3 : [12] } },
-            { "Index" : 8, "Image" : self.get_sprite((12 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [3], 1 : [29], 2 : [27], 3 : [36] } },
-            { "Index" : 9, "Image" : self.get_sprite((13 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [11], 3 : [] } },
-            { "Index" : 10, "Image" : self.get_sprite((15 * TILE_SIZE, 0 * TILE_SIZE)), "Entropy" : { 0 : [3], 1 : [36], 2 : [14], 3 : [36] } },
-            { "Index" : 11, "Image" : self.get_sprite((0 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [11, 5], 1 : [12], 2 : [11, 16], 3 : [] } },
-            { "Index" : 12, "Image" : self.get_sprite((1 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [11], 3 : [] } },
-            { "Index" : 13, "Image" : self.get_sprite((2 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [11], 3 : [] } },
-            { "Index" : 14, "Image" : self.get_sprite((4 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [26], 3 : [] } },
-            { "Index" : 15, "Image" : self.get_sprite((6 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [11], 3 : [] } },
-            { "Index" : 16, "Image" : self.get_sprite((7 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [11], 3 : [] } },
-            { "Index" : 17, "Image" : self.get_sprite((9 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [11], 1 : [12], 2 : [11], 3 : [36] } },
-            { "Index" : 18, "Image" : self.get_sprite((10 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [12], 1 : [24], 2 : [26], 3 : [24] } },
-            { "Index" : 19, "Image" : self.get_sprite((12 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 20, "Image" : self.get_sprite((13 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 21, "Image" : self.get_sprite((15 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 22, "Image" : self.get_sprite((16 * TILE_SIZE, 1 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 23, "Image" : self.get_sprite((0 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 24, "Image" : self.get_sprite((1 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [12], 1 : [25, 18], 2 : [], 3 : [23, 18] } },
-            { "Index" : 25, "Image" : self.get_sprite((2 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [13, 29], 1 : [], 2 : [], 3 : [24, 42] } },
-            { "Index" : 26, "Image" : self.get_sprite((4 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [14], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 27, "Image" : self.get_sprite((6 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 28, "Image" : self.get_sprite((7 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 29, "Image" : self.get_sprite((9 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [37, 32], 2 : [], 3 : [30, 39] } },
-            { "Index" : 30, "Image" : self.get_sprite((10 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 31, "Image" : self.get_sprite((11 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 32, "Image" : self.get_sprite((13 * TILE_SIZE, 3 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [36, 17], 2 : [26], 3 : [36, 7, 15] } },
-            { "Index" : 33, "Image" : self.get_sprite((15 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [36, 43, 37, 10], 2 : [26], 3 : [] } },
-            { "Index" : 34, "Image" : self.get_sprite((16 * TILE_SIZE, 2 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [26], 3 : [36, 32, 35, 10] } },
-            { "Index" : 35, "Image" : self.get_sprite((0 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [36, 45, 43, 32], 2 : [], 3 : [] } },
-            { "Index" : 36, "Image" : self.get_sprite((1 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [37, 43, 46, 34], 2 : [], 3 : [35, 32, 45, 33] } },
-            { "Index" : 37, "Image" : self.get_sprite((2 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [36, 32, 43, 45] } },
-            { "Index" : 38, "Image" : self.get_sprite((4 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 39, "Image" : self.get_sprite((6 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 40, "Image" : self.get_sprite((7 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 41, "Image" : self.get_sprite((9 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 42, "Image" : self.get_sprite((10 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 43, "Image" : self.get_sprite((12 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [3], 1 : [36, 37], 2 : [], 3 : [36, 32] } },
-            { "Index" : 44, "Image" : self.get_sprite((13 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [], 1 : [], 2 : [], 3 : [] } },
-            { "Index" : 45, "Image" : self.get_sprite((15 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [3, 33], 1 : [43, 36, 32], 2 : [], 3 : [] } },
-            { "Index" : 46, "Image" : self.get_sprite((16 * TILE_SIZE, 4 * TILE_SIZE)), "Entropy" : { 0 : [3, 34], 1 : [], 2 : [], 3 : [45, 36, 32] } },
+        SPRITES = [
+            Tile(0, 0, self.get_sprite(0, 0), ["AAA", "BBB", "CCC", "DDD"]),
+            Tile(1, 0, self.get_sprite(0, 0), ["AAA", "BBB", "CCC", "DDD"]).flip(True, False),
+            Tile(0, 1, self.get_sprite(0, 0), ["AAA", "BBB", "CCC", "DDD"]).flip(False, True),
+            Tile(1, 1, self.get_sprite(0, 0), ["AAA", "BBB", "CCC", "DDD"]).flip(True, True),
+            
+            # Tile(1, 0, self.get_sprite(1, 0), [], )
         ]
         self.initialize_generation()
     
-    def get_sprite(self, position):
-        x, y = position
+    def get_sprite(self, x, y):
         sprite = pygame.Surface((TILE_SIZE, TILE_SIZE))
         sprite.set_colorkey((0,0,0))
-        sprite.blit(self.sprite_sheet, (0, 0), (x, y, TILE_SIZE, TILE_SIZE))
+        sprite.blit(self.sprite_sheet, (0, 0), (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
         return sprite
 
     def process_sides(self, pxarray):
@@ -158,71 +137,18 @@ class Level:
         
         return neighbours
 
-
-    def initialize_sprites(self):
-        constraints= SPRITESHEET_LAYOUT["First_Layout"]["Constraints"]
-        for y in range(0, constraints["Height"], TILE_SIZE):
-            for x in range(0, constraints["Width"], TILE_SIZE):
-                image = self.get_sprite((x, y))
-                pxarray = self.convert_pixelarray(pygame.PixelArray(image))
-                if numpy.count_nonzero(pxarray) > 1:
-                    SPRITES.append({
-                        "Image" : image,
-                        "Pixels" : pxarray,
-                        "Entropy" : {
-                            0 : [], # top
-                            1 : [], # right
-                            2 : [], # down
-                            3 : []  # left
-                        }
-                    })
-
-
-        for sprite in SPRITES:
-            pxarray_sprite = sprite["Pixels"]
-            entropy_sprite = sprite["Entropy"]
-            top_sprite, right_sprite, down_sprite, left_sprite = pxarray_sprite[0], [pxsprite[31] for pxsprite in pxarray_sprite], pxarray_sprite[31], [pxsprite[0] for pxsprite in pxarray_sprite]
-            index, least_identical = 0, 24
-            for y in range(0, constraints["Height"], TILE_SIZE):
-                for x in range(0, constraints["Width"], TILE_SIZE):
-                    image = self.get_sprite((x, y))
-                    pxarray = self.convert_pixelarray(pygame.PixelArray(image))
-                    if numpy.count_nonzero(pxarray) > 1:
-                        top, right, down, left = pxarray[0], [array[31] for array in pxarray], pxarray[31], [array[0] for array in pxarray]
-                        # print(self.common_elements(pxarray_sprite, pxarray))
-                        print(len(self.common_sides(right_sprite, left)))
-                        if len(self.common_sides(top, down_sprite)) >= least_identical:
-                            entropy_sprite[ENTROPY_DICT[0]].append(index)
-                        if len(self.common_sides(right, left_sprite)) >= least_identical:
-                            entropy_sprite[ENTROPY_DICT[1]].append(index)
-                        if len(self.common_sides(down, top_sprite)) >= least_identical:
-                            entropy_sprite[ENTROPY_DICT[2]].append(index)
-                        if len(self.common_sides(left, right_sprite)) >= least_identical:
-                            entropy_sprite[ENTROPY_DICT[3]].append(index)
-                        
-                        if x == 32 * 6:
-                            sys.exit(1)
-                        index += 1
-            
-
-        
-        for sprite in SPRITES:
-            del sprite["Pixels"]
-            # print(SPRITES.index(sprite), sprite)
-
     def initialize_generation(self):
         # initialize grid and sprites into json array
-        # self.initialize_sprites()
         self.initialize_grid()
 
         # Initial state
-        arr = numpy.arange(15*15).reshape(15,15)
-        alist = [arr[0,:-1], arr[:-1,-1], arr[-1,::-1], arr[-2:0:-1,0]]
-        numpy.concatenate(alist)
-        for array in alist:
-            for index in array:
-                GRID[index].collapsed = True
-                GRID[index].options = [SPRITES[12]]
+        # arr = numpy.arange(15*15).reshape(15, 15)
+        # alist = [arr[0,:-1], arr[:-1,-1], arr[-1,::-1], arr[-2:0:-1,0]]
+        # numpy.concatenate(alist)
+        # for array in alist:
+        #     for index in array:
+        #         GRID[index].collapsed = True
+        #         GRID[index].options = [SPRITES[12]]
         
 
     def run(self, dt):
@@ -242,6 +168,7 @@ class Level:
         GRIDCOPY = list(filter(lambda x: x.collapsed == False, GRIDCOPY))
         GRIDCOPY.sort(key = lambda x : len(x.options))
         
+        GRIDCOPY = []
         if len(GRIDCOPY) > 0:
             length, stopIndex = len(GRIDCOPY[0].options), 0
             for i in range(1, len(GRIDCOPY), 1):
@@ -293,6 +220,10 @@ class Level:
             
             GRID = nextGrid
 
+        for sprite in SPRITES:
+            print(sprite.edges)
+            self.display_surface.blit(sprite.image, sprite.rect)
+        sys.exit(1)
         # print(SPRITES[0]["Entropy"])
         # self.stupid_check(SPRITES[0])
         self.level_sprites.draw(self.display_surface)
