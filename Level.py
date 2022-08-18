883f5182a3b769d3dd9c229aa33b19842b5dc6d5
WickedSs <slimane.ricardo@gmail.com>
2022-08-18 20:04:14 +0100

    using xml

--- Level.py
+++ Level.py
@@ -1,20 +1,15 @@
-from math import ceil
-from multiprocessing.resource_sharer import stop
-from pickle import TRUE
-from textwrap import indent
-from tkinter import Grid
-from tkinter.tix import IMAGE
-from turtle import onclick
 from pygame.math import Vector2
 import sys, os, pygame, numpy, random, time, string, operator
 from Settings import *
+import elementpath
+import xml.etree.ElementTree as ET
 
 
 ROOT = os.path.dirname(sys.modules['__main__'].__file__)
 SPRITES = []
 GRID = []
 VISITED = []
-DIM = 15
+DIM = 2
 
 
 class Tile:
@@ -32,24 +27,34 @@ class Tile:
         return arr.join('');
 
     def compareEdge(self, a, b):
-        return a == self.reverseString(b);
+        return len(list(set(a).intersection(b)));
 
     def analyze(self):
-        for i in range(SPRITES):
+        for i in range(len(SPRITES)):
             tile = SPRITES[i];
 
             # UP
-            if (self.compareEdge(tile.edges[2], self.edges[0])):
-                self.up.push(i);
+            vertical_tile, vertical_local = tile.edges[2].split(","), self.edges[0].split(",")
+            if (self.compareEdge(vertical_tile, vertical_local) > 0):
+                self.up.append(i);
+            
             # RIGHT
-            if (self.compareEdge(tile.edges[3], self.edges[1])):
-                self.right.push(i);
+            horizontal_tile, horizontal_local = tile.edges[3].split(","), self.edges[1].split(",")
+            if (self.compareEdge(horizontal_tile, horizontal_local) > 0):
+                self.right.append(i);
+            
             # DOWN
-            if (self.compareEdge(tile.edges[0], self.edges[2])):
-                self.down.push(i);
+            vertical_tile, vertical_local = tile.edges[0].split(","), self.edges[2].split(",") 
+            if (self.compareEdge(vertical_tile, vertical_local) > 0):
+                self.down.append(i);
+            
             # LEFT
-            if (self.compareEdge(tile.edges[1], self.edges[3])):
-                self.left.push(i);
+            horizontal_tile, horizontal_local = tile.edges[1].split(","), self.edges[3].split(",")
+            if (self.compareEdge(horizontal_tile, horizontal_local) > 0):
+                self.left.append(i);
+
+        
+        print(self.up, self.right, self.down, self.left)
     
 
     def rotate(self, angle):
@@ -63,21 +68,52 @@ class Tile:
     def flip(self, x_axis, y_axis):
         new_image = pygame.transform.flip(self.image, x_axis, y_axis)
         new_edges = self.edges
+
+        # horizontal axis flipping
         if x_axis:
-            new_edges[3], new_edges[1] = new_edges[1][::-1], new_edges[3][::-1]
+            # process left edges
+            edges_left, edge_right = new_edges[3].split(","), new_edges[1].split(",")
+            for cardinal in range(len(edges_left)):
+                edges_left[cardinal] = edges_left[cardinal][::-1]            
+            new_edges[3] = ",".join(input for input in edges_left)
+            
+            # process right edges
+            for cardinal in range(len(edge_right)):
+                edge_right[cardinal] = edge_right[cardinal][::-1]
+            new_edges[1] = ",".join(input for input in edge_right)
+            
+            # set the new edges
+            new_edges[3], new_edges[1] = new_edges[1], new_edges[3]
             new_edges[0], new_edges[2] = new_edges[0][::-1], new_edges[2][::-1]
+        
+        # vertical axis flipping
         if y_axis:
-            new_edges[0], new_edges[2] = new_edges[2][::-1], new_edges[0][::-1]
+            # process up edges
+            edges_up, edges_down = new_edges[0].split(","), new_edges[2].split(",")        
+            for cardinal in range(len(edges_up)):
+                edges_up[cardinal] = edges_up[cardinal].strip()[::-1]
+     
+            new_edges[0] = ",".join(input for input in edges_up)
+
+            # process down edges
+            for cardinal in range(len(edges_down)):
+                edges_down[cardinal] = edges_down[cardinal].strip()[::-1]
+            
+            new_edges[2] = ",".join(input for input in edges_down)
+            
+            # set the new edges
+            new_edges[0], new_edges[2] = new_edges[2], new_edges[0]
             new_edges[3], new_edges[1] = new_edges[3][::-1], new_edges[1][::-1]
             
         return Tile(self.x, self.y, new_image, new_edges)
 
+# E4$yP4$$c0d3
 
 class Cell:
     def __init__(self, index):
         self.index = index
         self.collapsed = False
-        self.options = [sprite for sprite in SPRITES]
+        self.options = [i for i in range(len(SPRITES))]
     
     def rotate(self):
         return
@@ -90,26 +126,31 @@ class Cell:
 class Level:
     spritesheet_filename = os.path.join(ROOT, "Assets/Spritesheet.png")
     example_file_01 = os.path.join(ROOT, "Assets/Example-01.png")
+    rules_file = os.path.join(ROOT, "Rules.xml")
     def __init__(self):
         global SPRITES
         self.display_surface = pygame.display.get_surface()
         self.sprite_sheet = pygame.image.load(self.spritesheet_filename).convert_alpha()
         self.sprite_sheet_rect = self.sprite_sheet.get_rect()
+        self.rules = ET.parse(self.rules_file)
         self.level_sprites = pygame.sprite.Group()
         self.layout_in_use = "First_Layout"
         self.index = 0
         SPRITES = [
 
-            # Tile(0, 0, self.get_sprite(1, 1), ["000", "000", "000", "000"]),
+            Tile(44, 0, self.get_sprite(1, 1), ["000, LLM", "000", "000", "000"]),
             # 0 0
-            Tile(0, 0, self.get_sprite(0, 0), ["ABB", "BCD", "EFF", "FFA"]),
-            Tile(1, 0, self.get_sprite(0, 0), ["ABB", "BCD", "EFF", "FFA"]).flip(True, False),
-            Tile(0, 1, self.get_sprite(0, 0), ["ABB", "BCD", "EFF", "FFA"]).flip(False, True),
-            Tile(1, 1, self.get_sprite(0, 0), ["ABB", "BCD", "EFF", "FFA"]).flip(True, True),
+            Tile(0, 0, self.get_sprite(0, 0), ["ABB, IHG", "JCD", "EFF", "FFA"]),
+            Tile(1, 0, self.get_sprite(0, 0), ["ABB, IHG", "JCD", "EFF", "FFA"]).flip(True, False),
+            Tile(0, 1, self.get_sprite(0, 0), ["ABB, IHG", "JCD", "EFF", "FFA"]).flip(False, True),
+            Tile(1, 1, self.get_sprite(0, 0), ["ABB, IHG", "JCD", "EFF", "FFA"]).flip(True, True),
             
             # # 1 0
-            # Tile(3, 0, self.get_sprite(1, 0), ["EEE", "BBB", "FFF", "DDD"]),
-            # Tile(3, 1, self.get_sprite(1, 2), ["FFF", "BBB", "EEE", "DDD"]),
+            Tile(3, 0, self.get_sprite(1, 0), ["BBB, IHG", "JCK, NCB, DCJ", "LLM, 000", "NCB, KCJ, JCD"]),
+            Tile(3, 1, self.get_sprite(1, 2), ["NON, 000", "KCJ", "BBB", "JCK"]),
+            
+            # Tile(3, 0, self.get_sprite(1, 6), ["GHI", "IJJ", "JJJ", "JJK"]),
+            # Tile(3, 1, self.get_sprite(1, 6), ["GHI", "IJJ", "JJJ", "JJK"]).flip(False, True),
 
             # # 0 1
             # Tile(5, 0, self.get_sprite(0, 1), ["CCC", "GGG", "CCC", "HHH"]),
@@ -171,76 +212,72 @@ class Level:
         
         return array
     
-    def common_sides(self, list1, list2):
-        return [element for element in list1 if element in list2]
-    
-    def common_elements(self, list1, list2):
-        max_identical = TILE_SIZE * TILE_SIZE
-        identical = 0
-        for y in range(len(list1)):
-            for x in range(len(list1[y])):
-                if list1[y][x] == list2[y][x]:
-                    identical += 1
+    def parse_rules(self):
+        root = self.rules.getroot()
+        for tile in root:
+            print(tile.tag, tile.attrib)
 
-        return (identical * 100) / max_identical
-    
+    def checkValid(self, array, valid):
+        for i in range(len(array)):
+            try:
+                element = array[i]
+                if element not in valid:
+                    array.remove(i) 
+            except (IndexError, ValueError) as e:
+                pass
+        
+        return array
+
+    def setup_adjacency(self):
+        for sprite in SPRITES:
+            sprite.analyze()
+
     def initialize_grid(self):
         for i in range(DIM * DIM):
             cell = Cell(index=i)
             GRID.append(cell)
 
-    def get_sprite_neighbours(self, x, y, array):
-        x, y, neighbours = int(x / TILE_SIZE), int(y / TILE_SIZE), array
-        if x > 0:
-            working_x, working_y = x - 1, y
-            neighbours[3].append((working_x, working_y))
-        
-        if x < (self.sprite_sheet_rect.width / TILE_SIZE) - 1:
-            working_x, working_y = x + 1, y
-            neighbours[1].append((working_x, working_y))
-        
-        if y > 0:
-            working_x, working_y = x, (y - 1) 
-            neighbours[0].append((working_x, working_y))
-
-        if y < (self.sprite_sheet_rect.height  / TILE_SIZE) - 1:
-            working_x, working_y = x, (y + 1)
-            neighbours[2].append((working_x, working_y))
-        
-        return neighbours
-
     def initialize_generation(self):
+
         # initialize grid and sprites into json array
-        self.initialize_grid()
+        self.parse_rules()
+        # self.initialize_grid()
+        # self.setup_adjacency()
+        sys.exit(1)
+
+        GRID[0].collapsed = True
+        GRID[0].options = [0]
 
         # Initial state
-        arr = numpy.arange(15*15).reshape(15, 15)
-        alist = [arr[0,:-1], arr[:-1,-1], arr[-1,::-1], arr[-2:0:-1,0]]
-        numpy.concatenate(alist)
-        for array in alist:
-            for index in array:
-                GRID[index].collapsed = True
-                GRID[index].options = [SPRITES[0]]
+        # arr = numpy.arange(15*15).reshape(15, 15)
+        # alist = [arr[0,:-1], arr[:-1,-1], arr[-1,::-1], arr[-2:0:-1,0]]
+        # numpy.concatenate(alist)
+        # for array in alist:
+        #     for index in array:
+        #         GRID[index].collapsed = True
+        #         GRID[index].options = [SPRITES[0]]
         
 
     def run(self, dt):
         global SPRITES, GRID, VISITED, DIM
-        self.display_surface.fill("black")
+        self.display_surface.fill("White")
 
-        # for y in range(DIM):
-        #     for x in range(DIM):
-        #         working_cell = GRID[x + y * DIM]
-        #         if working_cell.collapsed:
-        #             if working_cell.options:
-        #                 sprite = working_cell.options[0]
-        #                 self.display_surface.blit(sprite["Image"], (x * TILE_SIZE, y * TILE_SIZE))
+        for y in range(DIM):
+            for x in range(DIM):
+                working_cell = GRID[x + y * DIM]
+                if working_cell.collapsed:
+                    if working_cell.options:
+                        print(len(working_cell.options))
+                        sprite = working_cell.options[0]
+                        self.display_surface.blit(SPRITES[sprite].image, (x * TILE_SIZE, y * TILE_SIZE))
         
+
         #  pick cell with the least entropy
         GRIDCOPY = GRID.copy();
         GRIDCOPY = list(filter(lambda x: x.collapsed == False, GRIDCOPY))
         GRIDCOPY.sort(key = lambda x : len(x.options))
         
-        GRIDCOPY = []
+        # GRIDCOPY = []
         if len(GRIDCOPY) > 0:
             length, stopIndex = len(GRIDCOPY[0].options), 0
             for i in range(1, len(GRIDCOPY), 1):
@@ -262,42 +299,51 @@ class Level:
                     if GRID[index].collapsed:
                         nextGrid[index] = GRID[index]
                     else:
+                        options = [i for i in range(len(SPRITES))]
                         if y > 0:
-                            validSprites = []
+                            validOptions = []
                             lookup = GRID[x + (y - 1) * DIM]
-                            for sprite in lookup.options:
-                                validSprites.extend(sprite["Entropy"][ENTROPY_DICT[2]])
+                            for option in lookup.options:
+                                valid = SPRITES[option].down
+                                validOptions.extend(valid)
+                            options = self.checkValid(options, validOptions);
                         
                         if x < (DIM - 1):
-                            validSprites = []
+                            validOptions = []
                             lookright = GRID[( x + 1 ) + y  * DIM]
-                            for sprite in lookright.options:
-                                validSprites.extend(sprite["Entropy"][3])
+                            for option in lookright.options:
+                                valid = SPRITES[option].left
+                                validOptions.extend(valid)
+                            options = self.checkValid(options, validOptions);
                         
                         if y < (DIM - 1):
-                            validSprites = []
+                            validOptions = []
                             lookdown = GRID[x + (y + 1) * DIM]
-                            for sprite in lookdown.options:
-                                validSprites.extend(sprite["Entropy"][0])
+                            for option in lookdown.options:
+                                valid = SPRITES[option].up
+                                validOptions.extend(valid)
+                            options = self.checkValid(options, validOptions);
 
                         if x > 0:
-                            validSprites = []
+                            validOptions = []
                             lookleft = GRID[( x - 1 ) + y * DIM]
-                            for sprite in lookleft.options:
-                                validSprites.extend(sprite["Entropy"][1])
+                            for option in lookleft.options:
+                                valid = SPRITES[option].right
+                                validOptions.extend(valid)
+                            options = self.checkValid(options, validOptions);
                         
                         nextGrid[index] = Cell(index)
-                        nextGrid[index].collapsed = False
-                        nextGrid[index].options = [SPRITES[valid] for valid in validSprites]
+                        nextGrid[index].set_options(options)
+                        
             
             GRID = nextGrid
 
-        for sprite in SPRITES:
-            if self.index == 0:
-                print(sprite.edges)
-            self.display_surface.blit(sprite.image, sprite.rect)
+        # for sprite in SPRITES:
+        #     if self.index == 0:
+        #         print(sprite.edges)
+        #     self.display_surface.blit(sprite.image, sprite.rect)
         
-        self.index += 1
+        # self.index += 1
         # print(SPRITES[0]["Entropy"])
         # self.stupid_check(SPRITES[0])
         self.level_sprites.draw(self.display_surface)
--- Settings.py
+++ Settings.py
@@ -2,8 +2,8 @@ from turtle import width
 from pygame.math import Vector2
 
 
-SCREEN_WIDTH = 1440
-SCREEN_HEIGHT = 768
+SCREEN_WIDTH = 800 #1440
+SCREEN_HEIGHT = 600 #768
 TILE_SIZE = 32
 DIRECTIONS = [(0, -1), (1, 0), (0, 1), (-1, 0)] # (x, y) UP, RIGHT, DOWN, LEFT
 ENTROPY_DICT = { 0 : 2, 1 : 3, 2 : 0, 3 : 1}
